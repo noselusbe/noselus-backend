21ee28b61a08b065b5855bb9e14b6becb3e3010b
Nicolas Hurion <nicolas@hurion.eu>
2013-10-04 21:25:38 +0200

    Stub repository of questions

--- src/main/java/be/noselus/NosElus.java
+++ src/main/java/be/noselus/NosElus.java
@@ -1,5 +1,8 @@
 package be.noselus;
 
+import be.noselus.repository.QuestionRepository;
+import be.noselus.repository.QuestionRepositoryStub;
+import be.noselus.service.JsonTransformer;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -10,13 +13,23 @@ import static spark.Spark.get;
 
 public class NosElus {
 
+    public static QuestionRepository questionRepository = new QuestionRepositoryStub();
 
     public static void main(String[] args) throws IOException {
+
+
 
         get(new Route("/hello") {
             @Override
             public Object handle(Request request, Response response) {
                 return "<html><head></head><body>Hello World!</body></html>";
+            }
+        });
+
+        get(new JsonTransformer("questions") {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                return questionRepository.getQuestions();
             }
         });
 
