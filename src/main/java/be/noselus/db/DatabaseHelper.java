b9b39058832105a951f81b6d79cf9263f98c749f
Nicolas Hurion <nicolas@hurion.eu>
2013-10-05 19:50:37 +0200

    Allow to connect to db on heroku

--- src/main/java/be/noselus/db/DatabaseHelper.java
+++ src/main/java/be/noselus/db/DatabaseHelper.java
@@ -1,23 +1,44 @@
 package be.noselus.db;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseHelper {
 
-	public static Connection openConnection(boolean autoCommit, boolean readOnly) throws SQLException, ClassNotFoundException {
-		String url = "jdbc:postgresql://hackathon01.cblue.be:5432/noselus?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-		String user = "noselus2";
-		String password = "noselus";
-		
-		Class.forName("org.postgresql.Driver");
-		Connection db = DriverManager.getConnection(url, user, password);
-		
-		db.setAutoCommit(autoCommit);
-		db.setReadOnly(readOnly);
-		
-		return db;
-	}
-	
+    public static Connection openConnection(boolean autoCommit, boolean readOnly) throws SQLException, ClassNotFoundException {
+        final String database_url = System.getenv("DATABASE_URL");
+
+        String url;
+        String user;
+        String password;
+
+        if (database_url == null) {
+            url = "jdbc:postgresql://hackathon01.cblue.be:5432/noselus?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+            user = "noselus2";
+            password = "noselus";
+
+        } else {
+            URI dbUri = null;
+            try {
+                dbUri = new URI(database_url);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            user = dbUri.getUserInfo().split(":")[0];
+            password = dbUri.getUserInfo().split(":")[1];
+            url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        }
+
+        Class.forName("org.postgresql.Driver");
+        Connection db = DriverManager.getConnection(url, user, password);
+
+        db.setAutoCommit(autoCommit);
+        db.setReadOnly(readOnly);
+
+        return db;
+    }
+
 }
