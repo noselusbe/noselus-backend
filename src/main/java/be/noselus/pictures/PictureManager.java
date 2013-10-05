e5a19e851a269d7cc12d9353da83b05d1308d1ef
Nicolas Hurion <nicolas@hurion.eu>
2013-10-05 23:00:54 +0200

    Merge branch 'master' of https://bitbucket.org/oroger/egovhackathon
    Conflicts:
    	src/main/java/be/noselus/NosElus.java
    	src/main/java/be/noselus/pictures/PictureManager.java
    	src/test/java/be/noselus/pictures/PictureManagerTest.java

--- src/main/java/be/noselus/pictures/PictureManager.java
+++ src/main/java/be/noselus/pictures/PictureManager.java
@@ -12,64 +12,58 @@ import java.sql.SQLException;
 import java.util.Map;
 import java.util.TreeMap;
 
-import be.noselus.db.DatabaseHelper;
-
 public class PictureManager {
-	
-	private static PictureManager singleton = null;
-	
-	private static Map<Integer, Integer> mapping = null;
-	
-	private PictureManager() throws SQLException, ClassNotFoundException {
-		Connection db = DatabaseHelper.openConnection(false, true);
-		
-		PreparedStatement stat = db.prepareStatement("SELECT id, assembly_id FROM person;");
-		stat.execute();
-		
-		mapping = new TreeMap<Integer, Integer>();
-		while (stat.getResultSet().next()) {
-			int id = stat.getResultSet().getInt("id");
-			int assembly_id = stat.getResultSet().getInt("assembly_id");
-			mapping.put(id, assembly_id);
-		}
-		
-		stat.close();
-		db.close();
-	}
-	
-	public static PictureManager get() throws SQLException, ClassNotFoundException {
-		if (singleton == null) {
-			singleton = new PictureManager();
-		}
-		return singleton;
-	}
+
+
+    private Map<Integer, Integer> mapping = null;
 
     PoliticianRepository politicianRepository;
 
     @Inject
     public PictureManager(final PoliticianRepository politicianRepository) {
         this.politicianRepository = politicianRepository;
+        Connection db = null;
+        try {
+            db = DatabaseHelper.openConnection(false, true);
+
+            PreparedStatement stat = db.prepareStatement("SELECT id, assembly_id FROM person;");
+            stat.execute();
+
+            mapping = new TreeMap<Integer, Integer>();
+            while (stat.getResultSet().next()) {
+                int id = stat.getResultSet().getInt("id");
+                int assembly_id = stat.getResultSet().getInt("assembly_id");
+                mapping.put(id, assembly_id);
+            }
+
+            stat.close();
+            db.close();
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
     public InputStream get(int id) {
-		
-		Person politician = politicianRepository.getPoliticianById(id);
-		
-		String path = null;
-		String ext = null;
-		if (id >= 77 && id <= 150) {
-			path = "/pictures/parlement/";
-			ext = ".jpg";
-		} else if (id >= 849 && id <= 998) {
-			path = "/pictures/chamber/";
-			ext = ".gif";
-		}
 
-		if (path != null && ext != null) {
-			return PictureManager.class.getResourceAsStream(path + mapping.get(id) + ext);
-		} else {
-			return null;
-		}
-	}
-	
+        Person politician = politicianRepository.getPoliticianById(id);
+
+        String path = null;
+        String ext = null;
+        if (id >= 77 && id <= 150) {
+            path = "/pictures/parlement/";
+            ext = ".jpg";
+        } else if (id >= 849 && id <= 998) {
+            path = "/pictures/chamber/";
+            ext = ".gif";
+        }
+
+        if (path != null && ext != null) {
+            return PictureManager.class.getResourceAsStream(path + mapping.get(id) + ext);
+        } else {
+            return null;
+        }
+    }
+
 }
