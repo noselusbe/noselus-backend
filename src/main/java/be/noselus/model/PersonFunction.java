2e4237e265022e00e324960609b21bedadb2c22f
Nicolas Hurion <nicolas@hurion.eu>
2013-10-05 10:21:06 +0200

    caching deputies, start finding person by name

---
+++ src/main/java/be/noselus/model/PersonFunction.java
@@ -0,0 +1,6 @@
+package be.noselus.model;
+
+public enum PersonFunction {
+    DEPUTY,
+    MINISTER;
+}
--- src/main/java/be/noselus/repository/DeputyRepositoryInMemory.java
+++ src/main/java/be/noselus/repository/DeputyRepositoryInMemory.java
@@ -1,6 +1,10 @@
 package be.noselus.repository;
 
 import be.noselus.model.Person;
+import be.noselus.model.PersonFunction;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import com.google.common.io.Files;
 import com.google.common.io.LineProcessor;
 
@@ -10,21 +14,26 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class DeputyRepositoryInMemory implements DeputyRepository {
 
+    private List<Person> deputies;
+
     @Override
     public List<Person> getDeputies() {
+        if (deputies == null){
 
         try {
             URL url = getClass().getResource("/liste_parl_pw-2013-10-04.csv");
             File file = new File(url.toURI());
-            return Files.readLines(file, Charset.defaultCharset(),new LineProcessor<List<Person>>() {
+            deputies = Files.readLines(file, Charset.defaultCharset(),new LineProcessor<List<Person>>() {
 
                 final List<Person> result = new ArrayList<>();
                 int treatedLine=0;
+                int idCounter = 0;
 
                 @Override
                 public boolean processLine(final String s) throws IOException {
@@ -37,8 +46,9 @@ public class DeputyRepositoryInMemory implements DeputyRepository {
 
                     final String site = fields[8].trim();
 
-                    Person person = new Person(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], site);
+                    Person person = new Person(idCounter,fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], site, PersonFunction.DEPUTY);
                     result.add(person);
+                    idCounter++;
                     return true;
                 }
 
@@ -53,7 +63,27 @@ public class DeputyRepositoryInMemory implements DeputyRepository {
         } catch (URISyntaxException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-        return Collections.emptyList();
+
+        }
+        return deputies;
+    }
+
+    @Override
+    public Optional<Person> getDeputyByName(final String name) {
+        Predicate<Person> hasName = new Predicate<Person>() {
+            public boolean apply(Person p) {
+                return p != null && (name.equals(p.full_name) || p.full_name.contains(name));
+            }
+        };
+
+        Collection<Person> foundPerson = Collections2.filter(getDeputies(), hasName);
+
+        if (foundPerson.size() == 1){
+            return Optional.of(foundPerson.iterator().next());
+        } else if (foundPerson.size() > 1){
+            //trouble in paradise;
+        }
+        return Optional.absent();
     }
 
 }
