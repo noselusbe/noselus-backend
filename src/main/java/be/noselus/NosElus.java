89926210086a688f5d1af3cf3b89d3c12385fbb3
Nicolas Hurion <nicolas@hurion.eu>
2013-10-06 14:32:21 +0200

    search questions asked by

--- src/main/java/be/noselus/NosElus.java
+++ src/main/java/be/noselus/NosElus.java
@@ -1,17 +1,13 @@
 package be.noselus;
 
-import be.noselus.model.Person;
 import be.noselus.model.PersonSmall;
 import be.noselus.pictures.PictureManager;
 import be.noselus.repository.PoliticianRepository;
 import be.noselus.repository.QuestionRepository;
 import be.noselus.service.JsonTransformer;
-
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-
 import org.apache.commons.io.IOUtils;
-
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -20,7 +16,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.setPort;
 
 public class NosElus {
 
@@ -49,12 +46,14 @@ public class NosElus {
             @Override
             public Object myHandle(final Request request, final Response response) {
                 final String q = request.queryParams("q");
-                if (q == null){
-                    return questionRepository.getQuestions();
-                } else {
+                final String askedBy = request.queryParams("asked_by");
+                if (q != null) {
                     final String keywords = q.replace("\"", "");
                     return questionRepository.searchByKeyword(keywords.split(" "));
+                } else if (askedBy != null) {
+                    return questionRepository.questionAskedBy(Integer.valueOf(askedBy));
                 }
+                return questionRepository.getQuestions();
             }
         });
 
@@ -66,19 +65,19 @@ public class NosElus {
                 return questionRepository.getQuestionById(Integer.parseInt(params));
             }
         });
-        
+
         get(new JsonTransformer("/questions/askedBy/:name", "questions") {
-        	@Override
-        	public Object myHandle(final Request request, final Response response) {
-        		final String params = request.params(":name");
-        		List<PersonSmall> list = politicianRepository.getPoliticianByName(params);
-        		if (list.size() > 0) {
-        			return questionRepository.questionAskedBy(list.get(0).id);
-        		} else {
-        			return null;
-        		}
-        		
-        	}
+            @Override
+            public Object myHandle(final Request request, final Response response) {
+                final String params = request.params(":name");
+                List<PersonSmall> list = politicianRepository.getPoliticianByName(params);
+                if (list.size() > 0) {
+                    return questionRepository.questionAskedBy(list.get(0).id);
+                } else {
+                    return null;
+                }
+
+            }
         });
 
         get(new JsonTransformer("/politicians", "politicians") {
@@ -95,56 +94,56 @@ public class NosElus {
                 return politicianRepository.getPoliticianById(Integer.parseInt(params));
             }
         });
-        
+
         get(new JsonTransformer("/politicians/:id/askedBy", "questions") {
-        	@Override
-        	public Object myHandle(final Request request, final Response response) {
-        		final String params = request.params(":id");
-    			return questionRepository.questionAskedBy(Integer.valueOf(params));
-        	}
+            @Override
+            public Object myHandle(final Request request, final Response response) {
+                final String params = request.params(":id");
+                return questionRepository.questionAskedBy(Integer.valueOf(params));
+            }
         });
-        
+
         get(new Route("/politicians/picture/:id") {
-        	@Override
-        	public Object handle(final Request request, final Response response) {
-        		try {
-	        		final String id = request.params(":id");
-	        		byte[] out = null;
-	        		InputStream is = pictureManager.get(Integer.valueOf(id));
-	        		
-	        		if (is == null) {
-	        			response.status(404);
-	        			return null;
-	        		} else {
-		        		out = IOUtils.toByteArray(is);
-		        		response.raw().setContentType("image/jpeg;charset=utf-8");
-		        		response.raw().getOutputStream().write(out, 0, out.length);
-						return out;
-	        		}
-				} catch (NumberFormatException | IOException e) {
-					response.status(404);
-					return null;
-				}
-        	}
+            @Override
+            public Object handle(final Request request, final Response response) {
+                try {
+                    final String id = request.params(":id");
+                    byte[] out = null;
+                    InputStream is = pictureManager.get(Integer.valueOf(id));
+
+                    if (is == null) {
+                        response.status(404);
+                        return null;
+                    } else {
+                        out = IOUtils.toByteArray(is);
+                        response.raw().setContentType("image/jpeg;charset=utf-8");
+                        response.raw().getOutputStream().write(out, 0, out.length);
+                        return out;
+                    }
+                } catch (NumberFormatException | IOException e) {
+                    response.status(404);
+                    return null;
+                }
+            }
         });
-        
+
         get(new Route("/politicians/picture/:id/*/*") {
-        	@Override
-        	public Object handle(final Request request, final Response response) {
-        		try {
-        			final String id = request.params(":id");
-	        		int width = Integer.valueOf(request.splat()[0]);
-	        		int height = Integer.valueOf(request.splat()[1]);
-        			
-    				response.raw().setContentType("image/jpeg;charset=utf-8");
-    				pictureManager.get(Integer.valueOf(id),width, height, response.raw().getOutputStream());
-    				return null;
-        		} catch (NumberFormatException | IOException e) {
-        			response.status(404);
-        			return null;
-        		}
-        	}
+            @Override
+            public Object handle(final Request request, final Response response) {
+                try {
+                    final String id = request.params(":id");
+                    int width = Integer.valueOf(request.splat()[0]);
+                    int height = Integer.valueOf(request.splat()[1]);
+
+                    response.raw().setContentType("image/jpeg;charset=utf-8");
+                    pictureManager.get(Integer.valueOf(id), width, height, response.raw().getOutputStream());
+                    return null;
+                } catch (NumberFormatException | IOException e) {
+                    response.status(404);
+                    return null;
+                }
+            }
         });
-        
+
     }
 }
