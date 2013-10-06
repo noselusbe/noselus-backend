ea87603dd0f9f83704697df898a9cf3f67781a9d
Olivier Roger <olivier.roger@gmail.com>
2013-10-06 12:34:03 +0200

    Assembly enum

---
+++ src/main/java/be/noselus/model/AssemblyEnum.java
@@ -0,0 +1,19 @@
+package be.noselus.model;
+
+public enum AssemblyEnum {
+	
+	UN,
+	WAL,
+	FED;
+
+	public static AssemblyEnum getFromAssemblyId(int id) {
+		switch (id) {
+			case 1:
+			case 6:
+				return WAL;
+			default:
+				return FED;
+		}
+	}
+	
+}
