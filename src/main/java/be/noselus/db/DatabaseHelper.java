9634cfbcfa4d121becd66e81c10d6ab78c12013c
Julien Fastré <julien@fastre.info>
2013-10-14 16:43:35 +0200

    use SetEnv for db_password and db_user put in environnments variables : DATABASE_URL, DATABASE_USER and DATABASE_PASSWORD for connecting to DB properly.

--- src/main/java/be/noselus/db/DatabaseHelper.java
+++ src/main/java/be/noselus/db/DatabaseHelper.java
@@ -78,19 +78,20 @@ public class DatabaseHelper {
 
         public DbConfig invoke() {
             final String database_url = System.getenv("DATABASE_URL");
+            
 
             if (database_url == null) {
                 url = "jdbc:postgresql://localhost:5432/";
             } else {
-                URI dbUri = null;
+                
                 try {
-                    dbUri = new URI(database_url);
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                	user = System.getenv("DATABASE_USER");
+                	password = System.getenv("DATABASE_PASSWORD");
+                	url = database_url;
+                } catch (Exception e) {
+                	e.printStackTrace();
                 }
-                user = dbUri.getUserInfo().split(":")[0];
-                password = dbUri.getUserInfo().split(":")[1];
-                url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
             }
             return this;
         }
