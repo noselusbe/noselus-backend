fc0856ebfb0c5b4e0cb5b78e6bc57953e9bad30b
Julien Fastré <julien@fastre.info>
2013-10-14 20:50:37 +0200

    re-adding full URL

--- src/main/java/be/noselus/db/DatabaseHelper.java
+++ src/main/java/be/noselus/db/DatabaseHelper.java
@@ -83,15 +83,25 @@ public class DatabaseHelper {
             if (database_url == null) {
                 url = "jdbc:postgresql://localhost:5432/";
             } else {
-                
-                try {
-                	user = System.getenv("DATABASE_USER");
-                	password = System.getenv("DATABASE_PASSWORD");
-                	url = database_url;
-                } catch (Exception e) {
-                	e.printStackTrace();
-                }
-
+	        	user = System.getenv("DATABASE_USER");
+	        	password = System.getenv("DATABASE_PASSWORD");
+	        	url = database_url;
+	        	
+	        	if (user == null && password == null) {
+	        		URI dbUri = null;
+	                try {
+	                    dbUri = new URI(database_url);
+	                    user = dbUri.getUserInfo().split(":")[0];
+		                password = dbUri.getUserInfo().split(":")[1];
+		                url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+	                } catch (URISyntaxException e) {
+	                    e.printStackTrace(); //To change body of catch statement use File | Settings | File Templates.
+	                } catch (Exception e) {
+	                	e.printStackTrace();
+	                }
+	                
+	        	} 
+	        	
             }
             return this;
         }
