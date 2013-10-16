c29530c3ca2eeed7cec2328032acfcac3d43ddaa
Julien Fastré <julien@fastre.info>
2013-10-16 08:40:40 +0200

    SolrHelper - suite

--- src/main/java/be/noselus/search/SolrHelper.java
+++ src/main/java/be/noselus/search/SolrHelper.java
@@ -1,7 +1,11 @@
 package be.noselus.search;
 
+import java.io.IOException;
+
 import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.common.SolrInputDocument;
 
 public class SolrHelper {
 	
@@ -10,10 +14,39 @@ public class SolrHelper {
 	public static SolrServer getSolrServer() {
 		if (solrServer == null) {
 			String solrUrl = System.getenv("SOLR_URL");
-			solrServer = new HttpSolrServer(solrUrl);
+			
+			if (solrUrl == null){
+				// TODO add maven dependency solrServer = new EmbeddedSolrServer();
+			} else {
+				solrServer = new HttpSolrServer(solrUrl);
+			}
+			
+			
 		}
 		
 		return solrServer;
+	}
+	
+	public static void add(SolrInputDocument doc, boolean commitNow) {
+		try {
+			SolrHelper.getSolrServer().add(doc);
+		} catch (SolrServerException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (commitNow == true) {
+			try {
+				SolrHelper.getSolrServer().commit();
+			} catch (SolrServerException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void add(SolrInputDocument doc) {
+		SolrHelper.add(doc, false);
 	}
 
 }
