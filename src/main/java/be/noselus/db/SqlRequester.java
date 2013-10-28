59df036a4fb5faff4a8b70d75ee381e5e92a4a1d
Nicolas Hurion <nicolas@hurion.eu>
2013-10-28 18:50:25 +0100

    when importing either insert or update but don't duplicate data

--- src/main/java/be/noselus/db/SqlRequester.java
+++ src/main/java/be/noselus/db/SqlRequester.java
@@ -1,6 +1,8 @@
 package be.noselus.db;
 
 import be.noselus.model.Question;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,41 +10,94 @@ import java.sql.SQLException;
 
 public class SqlRequester {
 
-	public static void insertQuestion(Connection db, Question question) throws SQLException {
+    private static final Logger logger = LoggerFactory.getLogger(SqlRequester.class);
 
-		String sql = 
-				"INSERT INTO written_question (session, year, number, date_asked, date_answer, title, question_text, answer_text, asked_by, asked_to, assembly_ref) "
-				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		PreparedStatement stat = db.prepareStatement(sql);
-		int idx = 1;
-		stat.setString(idx++, question.session);
-		stat.setInt(idx++, question.year);
-		stat.setString(idx++, question.number);
-		stat.setDate(idx++, new java.sql.Date(question.date_asked.toDate().getTime()));
-		if (question.date_answered != null) {
-			stat.setDate(idx++, new java.sql.Date(question.date_answered.toDate().getTime()));
-		} else {
-			stat.setNull(idx++, java.sql.Types.DATE);
-		}
-		stat.setString(idx++, question.title);
-		stat.setString(idx++, question.question_text);
-		stat.setString(idx++, question.answer_text);
-		stat.setInt(idx++, question.asked_by);
-		stat.setInt(idx++, question.asked_to);
-		stat.setInt(idx++, question.id);
-		
-		stat.execute();
-		stat.close();
-	}
-	
-	public static void updatePersonAssemblyId(Connection db, String name, int id) throws SQLException {
-		String sql = "UPDATE person SET assembly_id = ? WHERE full_name LIKE ?;";
-		PreparedStatement stat = db.prepareStatement(sql);
-		
-		stat.setInt(1, id);
-		stat.setString(2, name.trim().replace(' ', ' '));
-		
-		stat.execute();
-		stat.close();
-	}
+    public static void insertOrUpdateQuestion(Connection db, Question question) throws SQLException {
+        if (questionIsPresent(db, question)) {
+            updateQuestion(db, question);
+        } else {
+            insertQuestion(db, question);
+        }
+
+    }
+
+    private static void insertQuestion(final Connection db, final Question question) throws SQLException {
+        logger.debug("Inserting question " + question.assembly.getLabel() + " " + question.id);
+
+        String sql =
+                "INSERT INTO written_question (session, year, number, date_asked, date_answer, title, question_text, answer_text, asked_by, asked_to, assembly_ref, assembly_id) "
+                        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement stat = db.prepareStatement(sql);
+        int idx = 1;
+        stat.setString(idx++, question.session);
+        stat.setInt(idx++, question.year);
+        stat.setString(idx++, question.number);
+        stat.setDate(idx++, new java.sql.Date(question.date_asked.toDate().getTime()));
+        if (question.date_answered != null) {
+            stat.setDate(idx++, new java.sql.Date(question.date_answered.toDate().getTime()));
+        } else {
+            stat.setNull(idx++, java.sql.Types.DATE);
+        }
+        stat.setString(idx++, question.title);
+        stat.setString(idx++, question.question_text);
+        stat.setString(idx++, question.answer_text);
+        stat.setInt(idx++, question.asked_by);
+        stat.setInt(idx++, question.asked_to);
+        stat.setString(idx++, question.id.toString());
+        stat.setInt(idx++, question.assembly.getId());
+
+        stat.execute();
+        stat.close();
+    }
+
+    private static void updateQuestion(final Connection db, final Question question) throws SQLException {
+        logger.debug("Updating question " + question.assembly.getLabel() + " " + question.id);
+
+        String sql =
+                "UPDATE written_question SET session = ?,  year = ? , number = ?, date_asked = ?, date_answer = ?, title = ?, question_text = ?, answer_text = ?, asked_by = ?, asked_to = ? "
+                        + "WHERE assembly_ref = ? AND assembly_id = ?";
+        PreparedStatement stat = db.prepareStatement(sql);
+        int idx = 1;
+        stat.setString(idx++, question.session);
+        stat.setInt(idx++, question.year);
+        stat.setString(idx++, question.number);
+        stat.setDate(idx++, new java.sql.Date(question.date_asked.toDate().getTime()));
+        if (question.date_answered != null) {
+            stat.setDate(idx++, new java.sql.Date(question.date_answered.toDate().getTime()));
+        } else {
+            stat.setNull(idx++, java.sql.Types.DATE);
+        }
+        stat.setString(idx++, question.title);
+        stat.setString(idx++, question.question_text);
+        stat.setString(idx++, question.answer_text);
+        stat.setInt(idx++, question.asked_by);
+        stat.setInt(idx++, question.asked_to);
+        stat.setString(idx++, question.id.toString());
+        stat.setInt(idx++, question.assembly.getId());
+
+        stat.execute();
+        stat.close();
+    }
+
+    private static boolean questionIsPresent(final Connection db, final Question question) throws SQLException {
+        logger.debug("Checking if question is present " + question.assembly.getLabel() + " " + question.id);
+        String sql = "SELECT id FROM written_question WHERE assembly_ref = ? AND assembly_id = ?";
+        PreparedStatement stat = db.prepareStatement(sql);
+        int idx = 1;
+        stat.setString(idx++, question.id.toString());
+        stat.setInt(idx++, question.assembly.getId());
+        stat.execute();
+        return stat.getResultSet().next();
+    }
+
+    public static void updatePersonAssemblyId(Connection db, String name, int id) throws SQLException {
+        String sql = "UPDATE person SET assembly_id = ? WHERE full_name LIKE ?;";
+        PreparedStatement stat = db.prepareStatement(sql);
+
+        stat.setInt(1, id);
+        stat.setString(2, name.trim().replace(' ', ' '));
+
+        stat.execute();
+        stat.close();
+    }
 }
--- src/main/java/be/noselus/scraping/ParliamentImporter.java
+++ src/main/java/be/noselus/scraping/ParliamentImporter.java
@@ -14,13 +14,13 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class ParlementImporter {
+public class ParliamentImporter {
 
-    private static final Logger logger = LoggerFactory.getLogger(ParlementImporter.class);
+    private static final Logger logger = LoggerFactory.getLogger(ParliamentImporter.class);
 
     public static final int WAIT_INTERVAL = 1500;
-    public static final int FROM_ID = 50061;
-    public static final int TO_ID = 50400;
+    public static final int FROM_ID = 50450;
+    public static final int TO_ID = 50800;
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Injector injector = Guice.createInjector(new NosElusModule());
@@ -30,17 +30,17 @@ public class ParlementImporter {
 
         String url = "http://parlement.wallonie.be/content/print_container.php?print=quest_rep_voir.php&type=all&id_doc=";
 
-        Connection db = DatabaseHelper.getInstance().getConnection(true, false);
 
-        for (int id = FROM_ID; id < TO_ID; id++) {
-            try {
-                logger.debug("importing question " + id);
-                SqlRequester.insertQuestion(db, parser.parse(id));
-                Thread.sleep(WAIT_INTERVAL);
-            } catch (IOException | IllegalArgumentException | InterruptedException e) {
-                logger.error(url + id, e);
+        try (Connection db = DatabaseHelper.getInstance().getConnection(true, false);) {
+            for (int id = FROM_ID; id < TO_ID; id++) {
+                try {
+                    logger.debug("importing question " + id);
+                    SqlRequester.insertOrUpdateQuestion(db, parser.parse(id));
+                    Thread.sleep(WAIT_INTERVAL);
+                } catch (IOException | IllegalArgumentException | InterruptedException | IndexOutOfBoundsException e) {
+                    logger.error(url + id, e);
+                }
             }
         }
-        db.close();
     }
 }
