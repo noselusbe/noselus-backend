707ba81c975f0c23503d7140e5171ade0d3d3834
Julien Fastré <julien@fastre.info>
2013-11-11 23:51:01 +0100

    search into question with solr

--- src/main/java/be/noselus/NosElus.java
+++ src/main/java/be/noselus/NosElus.java
@@ -2,15 +2,23 @@ package be.noselus;
 
 import be.noselus.model.Person;
 import be.noselus.model.PersonSmall;
+import be.noselus.model.Question;
 import be.noselus.pictures.PictureManager;
 import be.noselus.repository.PoliticianRepository;
 import be.noselus.repository.QuestionRepository;
+import be.noselus.search.SolrHelper;
 import be.noselus.service.JsonTransformer;
 
+import com.google.common.collect.Lists;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 
 import spark.Request;
 import spark.Response;
@@ -18,6 +26,8 @@ import spark.Route;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static spark.Spark.*;
@@ -51,8 +61,35 @@ public class NosElus {
                 final String q = request.queryParams("q");
                 final String askedBy = request.queryParams("asked_by");
                 if (q != null) {
-                    final String keywords = q.replace("\"", "");
-                    return questionRepository.searchByKeyword(keywords.split(" "));
+                    final String keywords = q.replace("\"", "").replace(" ", "+");
+                    
+                    SolrQuery parameters = new SolrQuery();
+                    parameters.set("q", "text:" + keywords);
+                    
+                    try {
+						QueryResponse resp = SolrHelper.getSolrServer().query(parameters);
+						
+						SolrDocumentList list = resp.getResults();
+						
+						List<Question> result = new ArrayList<Question>();
+						
+						Iterator<SolrDocument> i = list.iterator();
+						
+						while (i.hasNext()) {
+							String uriString = (String) i.next().getFieldValue("id");
+							//until now, we just query into question table, with id field
+							String[] uriDecomposed = uriString.split(":");
+							Integer id = Integer.valueOf(uriDecomposed[uriDecomposed.length - 1]);
+							result.add(questionRepository.getQuestionById(id));
+						}
+						
+						return result;
+						
+						
+					} catch (SolrServerException e) {
+						e.printStackTrace();
+					}
+                    
                 } else if (askedBy != null) {
                     return questionRepository.questionAskedBy(Integer.valueOf(askedBy));
                 }
