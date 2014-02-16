10cab981daa3899c7f9446f64d37f0b06e35ce53
Nicolas Hurion <nicolas@hurion.eu>
2014-02-16 11:10:54 +0100

    fix sonar violation

--- src/main/java/be/noselus/model/Question.java
+++ src/main/java/be/noselus/model/Question.java
@@ -71,75 +71,70 @@ public class Question implements HasIndexableDocument {
         this.eurovocs.add(eurovoc);
     }
 
-	@Override
-	public Map<Fields, Object> getIndexableFields() {
-		Map<SolrHelper.Fields, Object> doc = new HashMap<SolrHelper.Fields, Object>();
-		if (this.title != null ) {
-			doc.put(SolrHelper.StringFields.TITLE_FR, this.title);
-		}
-		if (this.questionText != null) {
-			doc.put(SolrHelper.StringFields.QUESTION_FR, this.questionText);
-		}
-		if (this.answerText != null) {
-			doc.put(SolrHelper.StringFields.ANSWER_FR, this.answerText);
-		}
-		if (this.dateAsked != null) {
-			doc.put(SolrHelper.DateFields.DATE_ASKED, this.dateAsked);
-		}
-		if (this.dateAnswered != null) {
-			doc.put(SolrHelper.DateFields.DATE_ANSWERED, this.dateAnswered);
-		}
-		if (this.assembly != null) {
-			doc.put(SolrHelper.StringFields.ASSEMBLY, this.assembly.getLabel());
-		}
-        if (this.askedBy > 0){
+    @Override
+    public Map<Fields, Object> getIndexableFields() {
+        Map<SolrHelper.Fields, Object> doc = new HashMap<SolrHelper.Fields, Object>();
+        if (this.title != null) {
+            doc.put(SolrHelper.StringFields.TITLE_FR, this.title);
+        }
+        if (this.questionText != null) {
+            doc.put(SolrHelper.StringFields.QUESTION_FR, this.questionText);
+        }
+        if (this.answerText != null) {
+            doc.put(SolrHelper.StringFields.ANSWER_FR, this.answerText);
+        }
+        if (this.dateAsked != null) {
+            doc.put(SolrHelper.DateFields.DATE_ASKED, this.dateAsked);
+        }
+        if (this.dateAnswered != null) {
+            doc.put(SolrHelper.DateFields.DATE_ANSWERED, this.dateAnswered);
+        }
+        if (this.assembly != null) {
+            doc.put(SolrHelper.StringFields.ASSEMBLY, this.assembly.getLabel());
+        }
+        if (this.askedBy > 0) {
             doc.put(SolrHelper.IntegerFields.ASKED_BY_ID, this.askedBy);
         }
 
-		return doc;
-	}
+        return doc;
+    }
 
-	@Override
-	public URI getURI() {
-		
-		if (this.id == null) {
-			throw new NullPointerException("Id must not be null");
-		}
-		
-		if (this.assembly == null) {
-			throw new NullPointerException("Assembly is null. Please avoid this!");
-		}
-		
-		String uriString = "urn:x-noselusbe:";
-		
-		//until now, we just have documents from parliement, waloon region and 
-		// parliement. Improve this when rewriting assembly class.
-		switch (this.assembly.getLevel()) {
-		case DEPUTY_CHAMBER:
-		case FEDERAL:
-			uriString = uriString + "federal.belgium.chamber:parliement";
-			break;
-		case REGION:
-			uriString = uriString + "region.wallonia:parliement";
-			break;
-		default:
-			try {
-				throw new Exception(String.valueOf(this.assembly.getLevel()) + " not supported yet");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		uriString = uriString + 
-				":written_question:" + this.id;
+    @Override
+    public URI getURI() {
 
-		URI u = URI.create(uriString);
-		return u;
-	}
+        if (this.id == null) {
+            throw new NullPointerException("Id must not be null");
+        }
 
-	@Override
-	public type getType() {
-		return HasIndexableDocument.type.WRITTEN_QUESTION;
+        if (this.assembly == null) {
+            throw new NullPointerException("Assembly is null. Please avoid this!");
+        }
 
-	}
+        String uriString = "urn:x-noselusbe:";
+
+        //until now, we just have documents from parliement, waloon region and
+        // parliement. Improve this when rewriting assembly class.
+        switch (this.assembly.getLevel()) {
+            case DEPUTY_CHAMBER:
+            case FEDERAL:
+                uriString = uriString + "federal.belgium.chamber:parliement";
+                break;
+            case REGION:
+                uriString = uriString + "region.wallonia:parliement";
+                break;
+            default:
+                throw new RuntimeException(String.valueOf(this.assembly.getLevel()) + " not supported yet");
+        }
+
+        uriString = uriString +
+                ":written_question:" + this.id;
+
+        return URI.create(uriString);
+    }
+
+    @Override
+    public type getType() {
+        return HasIndexableDocument.type.WRITTEN_QUESTION;
+
+    }
 }
