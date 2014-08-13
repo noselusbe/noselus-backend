be75245314f7e41c418a7e899be9de95cfae2b1d
Nicolas Hurion <nicolas@hurion.eu>
2014-08-13 20:24:23 +0200

    add caching to stop memory leak

--- src/main/java/be/noselus/repository/AssemblyRepositoryInDatabase.java
+++ src/main/java/be/noselus/repository/AssemblyRepositoryInDatabase.java
@@ -5,6 +5,9 @@ import be.noselus.util.dbutils.MapperBasedResultSetHandler;
 import be.noselus.util.dbutils.MapperBasedResultSetListHandler;
 import be.noselus.util.dbutils.QueryRunnerAdapter;
 import be.noselus.util.dbutils.ResultSetMapper;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Lists;
 import org.apache.commons.dbutils.ResultSetHandler;
 
 import javax.inject.Inject;
@@ -12,10 +15,13 @@ import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
 public class AssemblyRepositoryInDatabase implements AssemblyRepository, ResultSetMapper<Assembly> {
 
     private final QueryRunnerAdapter queryRunner;
+    private final Cache<Integer, Assembly> assemblies = CacheBuilder.newBuilder().build();
 
     @Inject
     public AssemblyRepositoryInDatabase(final DataSource dataSource) {
@@ -25,7 +31,17 @@ public class AssemblyRepositoryInDatabase implements AssemblyRepository, ResultS
     @Override
     public Assembly findId(final int id) {
         final ResultSetHandler<Assembly> assemblyResultSetHandler = new MapperBasedResultSetHandler<>(this);
-        final Assembly assembly = queryRunner.query("SELECT * FROM assembly WHERE id = ?;", assemblyResultSetHandler, id);
+        Assembly assembly;
+        try {
+            assembly = assemblies.get(id, new Callable<Assembly>() {
+                @Override
+                public Assembly call() throws Exception {
+                    return queryRunner.query("SELECT * FROM assembly WHERE id = ?;", assemblyResultSetHandler, id);
+                }
+            });
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
         if (assembly == null) {
             throw new RuntimeException("Error retrieving assembly with id " + id);
         }
@@ -35,7 +51,11 @@ public class AssemblyRepositoryInDatabase implements AssemblyRepository, ResultS
     @Override
     public List<Assembly> getAssemblies() {
         final ResultSetHandler<List<Assembly>> assemblyResultSetHandler = new MapperBasedResultSetListHandler<>(this);
-        return queryRunner.query("SELECT * FROM assembly;", assemblyResultSetHandler);
+        final List<Assembly> assemblyList = queryRunner.query("SELECT * FROM assembly;", assemblyResultSetHandler);
+        for (Assembly assembly : assemblyList) {
+            assemblies.asMap().putIfAbsent(assembly.getId(), assembly);
+        }
+        return Lists.newArrayList(assemblies.asMap().values());
     }
 
     @Override
