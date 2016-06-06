9b8f1dbab149dced41f5a0fdbb47c2d2c18767f5
Nicolas Hurion <nicolas@hurion.eu>
2016-06-06 21:15:51 +0200

    use HTTPS and disable ssl checks due to apparently now it's https only but certificate name does not match host.

--- src/main/java/be/noselus/scraping/WalloonRepresentativesFetcher.java
+++ src/main/java/be/noselus/scraping/WalloonRepresentativesFetcher.java
@@ -19,19 +19,29 @@ public class WalloonRepresentativesFetcher {
         WalloonRepresentativeListDocument listDocument;
         String normalizedName = Normalizer.normalize(name, Normalizer.Form.NFD).replace(' ', '+').replace(' ','+').replaceAll("[^\\p{ASCII}]", "");
 //        name = name.replace(' ', '+').replaceAll("[^A-Za-z0-9 /+]", "");
-        final String url = "http://www.parlement-wallon.be/pwpages?home_dep_search=" + normalizedName +
+        final String url = "https://www.parlement-wallon.be/pwpages?home_dep_search=" + normalizedName +
                 "&p=composition_dep";
         LOGGER.debug("Trying to find using url [{}]", url);
+        disableSSLCheckDueToBadCertificate();
         try (InputStream in = new URL(url).openStream()) {
             listDocument = parse(in, url);
         }
         return listDocument.getRepresentatives();
     }
 
+    /**
+     * Needed because the server issue a warning and java treat it as a fatal
+     * and because certificate name does not match host name
+     */
+    private void disableSSLCheckDueToBadCertificate() {
+        System.setProperty("jsse.enableSNIExtension", "false");
+        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+                (hostname, sslSession) -> true);
+    }
+
     protected WalloonRepresentativeListDocument parse(InputStream in, String url) throws IOException {
         Document doc = Jsoup.parse(in, "utf-8", url);
         return new WalloonRepresentativeListDocument(doc);
     }
-
 
 }
